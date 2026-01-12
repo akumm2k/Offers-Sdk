74d8435438e94c304bd3493c07cecce04a138d07
Aryan <82381989+akumm2k@users.noreply.github.com>
2026-01-12 21:45:52 +0100

    Use env var key constants in api config

--- src/offers_sdk/config.py
+++ src/offers_sdk/config.py
@@ -9,13 +9,21 @@ class ApiConfig:
     auth_endpoint: str
     refresh_token: str
 
+    BASE_URL_ENV_KEY = "OFFERS_API_BASE_URL"
+    AUTH_ENDPOINT_ENV_KEY = "AUTH_ENDPOINT"
+    REFRESH_TOKEN_ENV_KEY = "REFRESH_TOKEN"
+
     @classmethod
     def from_env(cls: Type[ApiConfig]) -> ApiConfig:
         try:
             return cls(
-                base_url=os.environ["OFFERS_API_BASE_URL"],
-                auth_endpoint=os.environ["AUTH_ENDPOINT"],
-                refresh_token=os.environ["REFRESH_TOKEN"],
+                base_url=os.environ[ApiConfig.BASE_URL_ENV_KEY],
+                auth_endpoint=os.environ[
+                    ApiConfig.AUTH_ENDPOINT_ENV_KEY
+                ],
+                refresh_token=os.environ[
+                    ApiConfig.REFRESH_TOKEN_ENV_KEY
+                ],
             )
         except KeyError as e:
             missing_var = e.args[0]
